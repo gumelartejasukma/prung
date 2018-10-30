2ff30bae06414f4f31a2348a7e808aa68bed2c6f
gumelarteja <kordon.kircon@gmail.com>
2018-10-30 16:47:00 +0700

    change app to index

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var app = require('./app');
+var app = require('./index');
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
