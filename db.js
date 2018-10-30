a540dc5b453cf5c9ba9ce1f844308791b7b99503
gumelarteja <kordon.kircon@gmail.com>
2018-10-30 20:51:45 +0700

    Unimplement morose

--- db.js
+++ db.js
@@ -1,2 +1,21 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:admin@cluster0-jofyq.gcp.mongodb.net/test?retryWrites=true');
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb+srv://admin:admin@cluster0-jofyq.gcp.mongodb.net/test?retryWrites=true');
+var db;
+
+var mongodb = require("mongodb");
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
