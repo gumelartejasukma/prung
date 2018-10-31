f9a4d831f82124c37be23dd7ed33626392d93b87
gumelarteja <kordon.kircon@gmail.com>
2018-11-01 03:27:31 +0700

    fix chat

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ function handleError(res, reason, message, code) {
    user.picture = body.user_picture;
    chat.user = user;
    chat.message = message;
-   db.collection(CHATS_COLLECTION+chat.event_id).insertOne(chat, function(err, doc) {
+   db.collection(CHATS_COLLECTION+body.event_id).insertOne(chat, function(err, doc) {
      if (err) {
        handleError(res, err.message, "Failed to create hashtag.");
      } else {
