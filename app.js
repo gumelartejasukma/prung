cc0c16f6480f457b7331edfa3e82cb232ed1d660
gumelarteja <kordon.kircon@gmail.com>
2018-10-31 17:14:09 +0700

    add event also to user

--- app.js
+++ app.js
@@ -204,6 +204,13 @@ function addEvent(req,res){
       handleError(res, err.message, "Failed to create new event.");
     } else {
       res.status(200).json(doc.ops[0]);
+      db.collection(USERS_COLLECTION).findOneAndUpdate({ _id: new ObjectID(body.user_id) },{$addToSet:{"events":events:doc.ops[0]._id}},function(err, doc) {
+        if (err) {
+          // handleError(res, err.message, "Failed to update event");
+        } else {
+          // res.status(200).json(doc);
+        }
+      });
     }
   });
 }
