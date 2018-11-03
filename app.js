52a284692d4ea1761a9f04dc73820ccbdc9ed639
gumelarteja <kordon.kircon@gmail.com>
2018-11-03 20:39:47 +0700

    S

--- app.js
+++ app.js
@@ -389,7 +389,6 @@ function fcmSend(token,res){
       title: '$GOOG up 1.43% on the day',
       body: '$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
     },
-    priority:'high',
     // data: {
     //   score: '850',
     //   time: '2:45'
